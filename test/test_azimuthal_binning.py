c5c4ecc2b0e0821216ae58de5083ab4959c52b72
Adam <aegger@slac.stanford.edu>
2020-04-14 10:33:00 -0700

    az bin init test working

--- test/test_azimuthal_binning.py
+++ test/test_azimuthal_binning.py
@@ -1,18 +1,19 @@
 import unittest
+import smalldata_tools.utilities as util
 from smalldata_tools.azimuthalBinning import azimuthalBinning as ab
 
 # Constants
 
 # Default values for keyword args
 DFLT_NAME = 'azav'
-DFLT_MASK = 'userMask'
-DFLT_GAIN_IMG = 'gainImg'
-DFLT_DARK_IMG = 'darkImg'
-DFLT_DEBUG = 'False'
+DFLT_MASK = None
+DFLT_GAIN_IMG = None
+DFLT_DARK_IMG = None
+DFLT_DEBUG = False
 DFLT_ADU_PER = 1.0
 DFLT_DIS_SAM = 100e-3
 DFLT_EBEAM = 9.5
-DFLT_LAM = DFLT_BEAM * 1e10
+DFLT_LAM = util.E2lam(DFLT_EBEAM) * 1e10
 DFLT_PHI_BINS = 1
 DFLT_PPLANE = 0
 DFLT_TX = 0.
@@ -30,7 +31,7 @@ class TestAzimuthatlBinning(unittest.TestCase):
     """Test AzimuthalBinning class"""
     def test_default_props(self):
         azb = ab()
-        self.assertEqual(azb.name, DEFAULT_NAME)
+        self.assertEqual(azb.name, DFLT_NAME)
         self.assertEqual(azb.mask, DFLT_MASK)
         self.assertEqual(azb.gainImg, DFLT_GAIN_IMG)
         self.assertEqual(azb.darkImg, DFLT_DARK_IMG)
